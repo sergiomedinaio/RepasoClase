1f5b88db0f832e81da4c901e0b02d4493a642719
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-11 22:57:39 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/davinci/repasoclase/LoginActivity.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.repasoclase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_prueba);
+    }
+}
